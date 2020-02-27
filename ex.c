368488c9729a8639b56291887cce8eabe2841871
Brandon-Amezcua <42852698+Brandon-Amezcua@users.noreply.github.com>
2020-02-26 17:48:34 -0800

    Create ex.c

---
+++ ex.c
@@ -0,0 +1,41 @@
+#include  <stdio.h>
+#include <string.h>
+
+void print_reverse(const char* s) {
+  if (*s == '\0') {return;}
+  print_reverse(s + 1);
+  printf("%c", *s);
+}
+
+char* strcpy (char* s, const char* t) {
+  char * p = s;
+  while ((*s++ = *t++) != '\0') {}
+  return p;
+}
+
+char* strncpy(char* s, const char* t, size_t n) {
+  char* p = s;
+  while( n-- > 0 && ((*s++ = *t++) != '\0' )) {}
+  while ( n-- > 0 ) {
+     *s++ = '\0';
+  }
+  *s = '\0';
+  return p;
+}
+
+int strcmp(const char* s, const char* t) {
+  while(*s != '\0' && *t != '\0' && *s == *t) {
+    ++s;
+    ++t;
+  }
+  return *s - *t;
+}
+
+int main () {
+  const char* line = "abcde";
+  char* copy = "cat";
+  print_reverse(line);
+  strncpy(copy, line, 4);
+
+  printf("\ncopy = %s\nline = %s\n", copy, line);
+}
