29750628626b926dfba743f52884da08dd77a0ed
Brandon-Amezcua <brandonamezcua@csu.fullerton.edu>
2020-03-01 21:41:14 -0800

    fixed the strend function

--- ex.c
+++ ex.c
@@ -56,11 +56,31 @@ char* strncat(char* s, const char* t, size_t n) {
   return strcpy(s, t);
 }
 
+int strend(char* s, char* t){
+  int end = 0;
+  int slen = 0;
+  int tlen = 0;
+  slen = strlen(s);
+  tlen = strlen(t);
+  if (tlen <= slen) {
+    s += slen - tlen;
+
+    if (strcmp(s, t) == 0) {
+      end = 1;
+    }
+  }
+  return end;
+}
+
 int main () {
   const char* line = "abcde";
   const char* second = "abcdef";
   const char* add = "Hello->";
+  char* beg = "Beginning";
+  char* end = "ing";
+  char* not = "not";
   char buf[50];
+
   print_reverse(line);
   strcpy(buf, line);
   printf("\n\ncopy = %s\nline = %s\n", buf, line);
@@ -80,5 +100,11 @@ int main () {
 
   printf("strncmp = %d\nline = %s\nline = %s\n\n",strncmp(line,line, 6), line, line);
 
-  printf("strcat = %s\nbuf = %s\nstrcat 2 = %s\nbuf = %s\n",strcat(buf, line), buf, strcat(buf,add), buf);
+  printf("strcat = %s\nbuf = %s\nstrcat 2 = %s\nbuf = %s\n\n",strcat(buf, line), buf, strcat(buf,add), buf);
+
+  printf("strend = %d\nbeg = %s\nend = %s\n\n",strend(beg, end), beg, end);
+
+  printf("strend = %d\nend = %s\nbeg = %s\n\n",strend(end, beg), end, beg); 
+
+  printf("strend = %d\nnot = %s\nbeg = %s\n",strend(not, beg), not, beg);
 }
